0f91f8e6b884a05541220eb59f956f36b46dad8a
Sean <norwood.sean@gmail.com>
2016-04-04 17:06:45 -0500

    Setup shell Add Reference

---
+++ src/main/java/com/stiforr/swnmod/Reference/References.java
@@ -0,0 +1,8 @@
+package com.stiforr.swnmod.Reference;
+
+public interface References {
+    String MOD_ID = "swnmod";
+    String VERSION = "0.0.1";
+    String NAME = "SWNMod";
+
+}
--- src/main/java/com/stiforr/swnmod/SWNMod.java
+++ src/main/java/com/stiforr/swnmod/SWNMod.java
@@ -1,4 +1,30 @@
 package com.stiforr.swnmod;
 
+import com.stiforr.swnmod.Reference.References;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = References.MOD_ID, version = References.VERSION, name = References.NAME)
 public class SWNMod {
+
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public static void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public static void postInit(FMLPostInitializationEvent event){
+
+    }
+
+
+
 }
