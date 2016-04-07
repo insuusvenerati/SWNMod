54c2ea4d6b74cfedd96140e85cf9245ac220124a
Sean <norwood.sean@gmail.com>
2016-04-06 20:54:53 -0500

    Getting setup for MC 1.9

--- src/main/java/com/stiforr/swnmod/Reference/References.java
+++ src/main/java/com/stiforr/swnmod/Reference/References.java
@@ -2,7 +2,7 @@ package com.stiforr.swnmod.reference;
 
 public interface References {
     String MOD_ID = "swnmod";
-    String VERSION = "0.0.1";
+    String VERSION = "1.9-0.0.1";
     String NAME = "SWNMod";
     String CLIENT_PROXY = "com.stiforr.swnmod.proxy.ClientProxy";
     String SERVER_PROXY = "com.stiforr.swnmod.proxy.ServerProxy";
