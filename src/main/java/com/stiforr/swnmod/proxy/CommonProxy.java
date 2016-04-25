e86becd0c7fc4527eff092e7cbbe548434003e37
FusionLord <FusionLord@live.com>
2016-04-25 16:55:49 -0700

    Fixed the Inventory Icons with the help of WillieWillus

--- src/main/java/com/stiforr/swnmod/proxy/CommonProxy.java
+++ src/main/java/com/stiforr/swnmod/proxy/CommonProxy.java
@@ -1,5 +1,7 @@
 package com.stiforr.swnmod.proxy;
 
+import com.stiforr.swnmod.init.ModBlocks;
+import com.stiforr.swnmod.init.ModItems;
 import com.stiforr.swnmod.world.SWNModWorldGen;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -9,7 +11,10 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class CommonProxy{
 
     public void preInit(FMLPreInitializationEvent e){
-
+		ModItems.init();
+		ModItems.registerItems();
+		ModBlocks.init();
+		ModBlocks.registerBlocks();
     }
 
     public void init(FMLInitializationEvent e){
